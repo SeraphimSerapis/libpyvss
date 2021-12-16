f7076e54631091f95d2f68703376441214f4cb52
Tim Messerschmidt <timmesserschmidt@gmail.com>
2021-12-16 13:57:39 +0100

    More tweaks

--- setup.py
+++ setup.py
@@ -9,9 +9,14 @@ here = path.abspath(path.dirname(__file__))
 setup(
     name='vss-python-api',
     version='0.9.7',
-    url='https://github.com/SeraphimSerapis/libpyvss.git',
+    url='https://github.com/SeraphimSerapis/libpyvss',
     author='Tim Messerschmidt',
+    author_email="timmesserschmidt+vss@gmail.com",
     description='A fork of the official libpyvss',
     packages=find_packages(),
-    install_requires=['requests', 'requests-toolbelt', 'python-dotenv'],
+    install_requires=[
+        'requests==2.26.0',
+        'requests-toolbelt==0.9.1',
+        'python-dotenv==0.19.2'
+    ]
 )
